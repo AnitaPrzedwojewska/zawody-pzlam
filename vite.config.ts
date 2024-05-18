6414ad86415ed84658ace5103181409d989b85ea
AnitaPrzedwojewska <nisiek@wp.pl>
2024-05-18 23:50:07 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/zawody-pzlam",
   plugins: [react()],
   server: {
     open: true,
